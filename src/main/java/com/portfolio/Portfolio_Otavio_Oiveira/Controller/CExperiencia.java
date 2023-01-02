826f8d462ec6e956609267695e0c53af7be080ce
OliveiraOtavio <otavio.oliveira@msn.com>
2023-01-02 10:51:56 -0300

    Configuracion Parametros

---
+++ src/main/java/com/portfolio/Portfolio_Otavio_Oiveira/Controller/CExperiencia.java
@@ -0,0 +1,82 @@
+
+package com.portfolio.Portfolio_Otavio_Oiveira.Controller;
+
+import com.portfolio.Portfolio_Otavio_Oiveira.Dto.dtoExperiencia;
+import com.portfolio.Portfolio_Otavio_Oiveira.Entity.Experiencia;
+import com.portfolio.Portfolio_Otavio_Oiveira.Security.Controller.Mensaje;
+import com.portfolio.Portfolio_Otavio_Oiveira.Service.SExperiencia;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("explab")
+@CrossOrigin(origins = "http://localhost:4200")
+public class CExperiencia {
+    
+    @Autowired
+    SExperiencia sExperiencia;
+    
+    @GetMapping ("/lista")
+    public ResponseEntity <List <Experiencia> > list() {
+        List <Experiencia> list = sExperiencia.list();
+        return new ResponseEntity (list, HttpStatus.OK);
+    }
+    
+    @PostMapping ("/create")
+    public ResponseEntity <?> create (@RequestBody dtoExperiencia dtoexp) {
+        if (StringUtils.isBlank(dtoexp.getNombreE()))
+            return new ResponseEntity (new Mensaje ("El nombre es obligatorio!"), HttpStatus.BAD_REQUEST);
+        
+        if (sExperiencia.existsByNombreE(dtoexp.getNombreE()))
+            return new ResponseEntity(new Mensaje ("Esa experiencia ya existe!"), HttpStatus.BAD_REQUEST);
+        
+        Experiencia experiencia = new Experiencia (dtoexp.getNombreE(), dtoexp.getDescripcionE());
+        sExperiencia.save(experiencia);
+        
+        return new ResponseEntity (new Mensaje ("Experiencia agregada con exito!"), HttpStatus.OK);
+    }
+    
+    @PutMapping ("/update/{id}")
+    public ResponseEntity <?> update (@PathVariable("id") int id, @RequestBody dtoExperiencia dtoexp) {
+        // Validacion de la existencia del ID
+        if (!sExperiencia.existsById(id))
+            return new ResponseEntity (new Mensaje ("El ID no existe!"), HttpStatus.BAD_REQUEST);
+        
+        // Comparacion del nombre de las experiencias
+        if (sExperiencia.existsByNombreE(dtoexp.getNombreE()) && sExperiencia.getByNombreE(dtoexp.getNombreE()).get().getId() != id)
+            return new ResponseEntity (new Mensaje ("Esa experiencia ya existe!"), HttpStatus.BAD_REQUEST);
+        
+        // Averigua si el campo esta vacio
+        if (StringUtils.isBlank(dtoexp.getNombreE()))
+            return new ResponseEntity (new Mensaje ("El nombre es obligatorio!"), HttpStatus.BAD_REQUEST);
+        
+        Experiencia experiencia = sExperiencia.getOne(id).get();
+        experiencia.setNombreE(dtoexp.getNombreE());
+        experiencia.setDescripcionE((dtoexp.getDescripcionE()));
+        
+        sExperiencia.save(experiencia);
+        return new ResponseEntity (new Mensaje ("Experiencia actualizada!"), HttpStatus.OK);
+    }
+    
+    public ResponseEntity <?> delete (@PathVariable("id") int id) {
+        
+        // Validacion de la existencia del ID
+        if (!sExperiencia.existsById(id))
+            return new ResponseEntity (new Mensaje ("El ID no existe!"), HttpStatus.BAD_REQUEST);
+        
+        sExperiencia.delete(id);
+        
+        return new ResponseEntity (new Mensaje ("Experiencia eliminada!"), HttpStatus.OK);
+    }
+}
