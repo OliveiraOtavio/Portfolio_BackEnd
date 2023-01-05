61689175e627ac420304349e9e3b61c04193fd3e
OliveiraOtavio <otavio.oliveira@msn.com>
2023-01-05 12:19:37 -0300

    Update WebConfig.java

--- src/main/java/com/portfolio/Portfolio_Otavio_Oiveira/WebConfig.java
+++ src/main/java/com/portfolio/Portfolio_Otavio_Oiveira/WebConfig.java
@@ -1,27 +1,18 @@
-/* package com.portfolio.Portfolio_Otavio_Oiveira;
-
+package com.portfolio.Portfolio_Otavio_Oiveira;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer; 
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-   
-    public WebMvcConfigurer CORSConfigurer() {
-        return new WebMvcConfigurer() {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
 
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
     }
 
 }
-*/
