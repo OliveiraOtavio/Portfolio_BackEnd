e30e41b8c8993ac3c298167797c77a3f82231645
OliveiraOtavio <otavio.oliveira@msn.com>
2023-01-05 13:29:25 -0300

    Update WebConfig.java

--- src/main/java/com/portfolio/Portfolio_Otavio_Oiveira/WebConfig.java
+++ src/main/java/com/portfolio/Portfolio_Otavio_Oiveira/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("https://portfolio-designhaus.koyeb.app/auth/");
+        registry.addMapping("/**");
 
     }
 
