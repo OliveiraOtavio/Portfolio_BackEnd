edc799e0712c0a977f8ed19079515a7bbf759f89
OliveiraOtavio <otavio.oliveira@msn.com>
2023-01-05 09:55:52 -0300

    Create WebConfig.java

---
+++ src/main/java/com/portfolio/Portfolio_Otavio_Oiveira/WebConfig.java
@@ -0,0 +1,18 @@
+
+package com.portfolio.Portfolio_Otavio_Oiveira;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings (CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+    
+}
